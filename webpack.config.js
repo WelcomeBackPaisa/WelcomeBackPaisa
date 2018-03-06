b684c6c123790665e2b0d78cb178e67a55afde74
Bruno <bruno.torres.fabila@gmail.com>
2018-03-05 19:56:15 -0600

    Change dB credentials, edit webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -9,15 +9,15 @@ module.exports = {
     path: DIST_DIR
   },
   module : {
-    loaders : [
+    rules : [
       {
         test : /\.jsx?/,
         include : SRC_DIR,
         loader : 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'env']
        }
-      }
-    ]
+     }
+   ]
   }
 };
