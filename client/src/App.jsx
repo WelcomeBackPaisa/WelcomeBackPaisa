21f8ae508a5ed27fa7a250117437a177bf426cdc
Jacqueline930 <jacqueline.lopez.930@gmail.com>
2018-03-08 19:27:53 -0600

    just added correct identation and spacing on routes in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,21 @@
   App will render:
     HeaderBar & Menu, which will be shared across all pages
   Routes rendered inside App will be:
-    Forum ("/")
+    Forum ("/forum")
     Post ("/post/:id")
     About Me ("/about")
 */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 
+import '../dist/scss/main.scss';
 import {BrowserRouter, Route} from 'react-router-dom';
 import {MuiThemeProvider} from 'material-ui/styles';
 
 //SubComponents that will be rendered
+import HeaderBar from "./components/app/HeaderBar.jsx";
 import Forum from './pages/Forum.jsx';
 import Post from './pages/Post.jsx';
 import AboutMe from './pages/AboutMe.jsx';
@@ -21,6 +24,18 @@ import AboutMe from './pages/AboutMe.jsx';
 //All GET/POST logic will be moved to the corresponding page
 //TODO Add Router Route
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      posts: [
+        { id: 54,
+          title: 'help I need somebody',
+          body: 'really need somebody'},
+        { id: 55,
+          title: 'CDMX is great',
+         body: 'Best city ever'}
+      ],
+    currentPost: null
 
 
  render() {
@@ -28,13 +43,13 @@ class App extends React.Component{
       <div>
         <BrowserRouter>
           <MuiThemeProvider>
-            <main>
-              <Switch>
-                <Route path="/forum" component={Forum} />
-                <Route path="/post/:id" component={Post} />
-                <Route path="/about" component={AboutMe} />
-              </Switch>
-            </main>
+          <main>
+            <Switch>
+              <Route path="/" component={Forum} />
+              <Route path="/post/:id" component={Post} />
+              <Route path="/about" component={AboutMe} />
+            </Switch>
+          </main>
           </MuiThemeProvider>
         </BrowserRouter>
       </div>
